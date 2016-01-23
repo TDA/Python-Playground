abf028e04a0b7a5db947f3e400aeee33cf2d447a
Sai Pc <schand31@asu.edu>
2016-01-23 16:32:41 -0700

    printing sys data

---
+++ sys_outs_2.py
@@ -0,0 +1,10 @@
+__author__ = 'saipc'
+
+import sys
+print sys.platform
+print sys.api_version
+print sys.flags # also includes the other vars :D
+print sys.byteorder # big or little endian?
+print sys.dont_write_bytecode # COOL!
+print sys.modules
+print sys.path
