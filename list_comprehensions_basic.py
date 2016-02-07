320b3597a2af446e62dc91d0d5e163831b6677e7
Sai Pc <schand31@asu.edu>
2016-02-07 16:24:55 -0700

    added filter

--- list_comprehensions_basic.py
+++ list_comprehensions_basic.py
@@ -4,6 +4,10 @@ matrix = [ [1, 2, 3],
            [4, 5, 6],
            [7, 8, 9],]
 
+
+def even_filter(x):
+    return x % 2 == 0
+
 print(matrix)
 x = []
 for row in matrix:
@@ -11,3 +15,7 @@ for row in matrix:
 print(x)
 odd_nos = [elt for elt in x if elt % 2 == 1]
 print(odd_nos)
+
+# with filter
+even_nos = filter(even_filter, x)
+print(even_nos)
