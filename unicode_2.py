15f025d907f4c2fca203fa0532510dd9db92f20d
Sai Pc <schand31@asu.edu>
2016-02-07 15:42:06 -0700

    added unicode

---
+++ unicode_2.py
@@ -0,0 +1,5 @@
+__author__ = 'saipc'
+
+string2 = u's\u00c4i'
+print string2
+
---
+++ unicode_3.py
@@ -0,0 +1,11 @@
+__author__ = 'saipc'
+
+string3 = b's\xc4i'
+print string3
+string = "s\xc4i"
+print string
+
+print(string == string3)
+
+string4 = u"s\xc4i"
+print(string4)
