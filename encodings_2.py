0b482823c67ebbb170b89f4640a07a1a7090b8af
Sai Pc <schand31@asu.edu>
2016-02-10 12:55:35 -0700

    byte encodings 2 n 3

---
+++ encodings_2.py
@@ -0,0 +1,6 @@
+__author__ = 'saipc'
+
+# will all print the same thing
+print("ssss")
+print(b"ssss")
+print(r"ssss")
---
+++ encodings_3.py
@@ -0,0 +1,5 @@
+__author__ = 'saipc'
+
+print("ssss") # will print 'ssss'
+print(b"ssss") # will print b'ssss'
+print(r"ssss") # will print 'ssss'
